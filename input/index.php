2fc686aa56e32a46519980eadebb0ae9ac7701fc
Shawn Schwartz <shawnschwartz@ucla.edu>
2020-07-25 21:47:07 -0700

    reverse IO index.php contents

--- input/index.php
+++ input/index.php
@@ -1,4 +1,4 @@
 <?php
-    echo "<h1><em>sashimi</em> OUTPUT DIRECTORY</h1>";
-    echo "<p>Please visit <a href='../download.php'>../download.php</a> to view segmented images and download training data.</p>"
+    echo "<h1><em>sashimi</em> INPUT DIRECTORY</h1>";
+    echo ""
 ?>
